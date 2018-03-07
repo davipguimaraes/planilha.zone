bf0ba9cba7ff110f92ed512c514bcc7000e5b50e
Davi P. Guimaraes <davi@digitalm3.com.br>
2018-03-07 08:58:04 -0300

    Add cabeçalho

--- libs/config.php
+++ libs/config.php
@@ -1,13 +1,17 @@
 <?php
 
 $CORREIOS_CONTRATO = array(
-    array('04669','Correios - PAC - Código Serviço 04669',1),
-    array('04162','Correios - SEDEX - Código Serviço 04162',1),
-    array('04510','Correios - PAC - Código Serviço 04510',0),
-    array('04014','Correios - SEDEX - Código Serviço 04014',0),
-    array('04812','Correios - PAC - Código Serviço 04812',1),
-    array('04316','Correios - SEDEX - Código Serviço 04316',1),
-    array('jadlog','Jadlog - % de venda configurado na plataforma',1)
+	array('','---- CORREIOS COM CONTRATO À PARTIR DE 2017 (v.2018) ----','',false),
+    array('04669','Correios - PAC - Código Serviço 04669',1,true),
+	array('04162','Correios - SEDEX - Código Serviço 04162',1,true),
+	array('','---- CORREIOS SEM CONTRATO À PARTIR DE 2017  (v.2018) ----','',false),
+    array('04510','Correios - PAC - Código Serviço 04510',0,true),
+	array('04014','Correios - SEDEX - Código Serviço 04014',0,true),
+	array('','---- CORREIOS COM CONTRATOS À PARTIR DE 2017  (v.2018) - INDUSTRIAL ----','',false),
+    array('04812','Correios - PAC - Código Serviço 04812',1,true),
+	array('04316','Correios - SEDEX - Código Serviço 04316',1,true),
+	array('','---- JADLOG ----','',false),
+    array('jadlog','Jadlog - % de venda configurado na plataforma',1,true)
 );
 
 ?>
