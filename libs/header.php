4d77539dc0996efd735438f8e90267269b439974
Davi Porto <daviporto211@yahoo.com>
2018-03-06 23:19:01 -0300

    removido tag do google analitics

--- libs/header.php
+++ libs/header.php
@@ -20,15 +20,6 @@
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
     
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-51158860-4', 'planilha.zone');
-  ga('send', 'pageview');
-</script>
 
     <style>
 
